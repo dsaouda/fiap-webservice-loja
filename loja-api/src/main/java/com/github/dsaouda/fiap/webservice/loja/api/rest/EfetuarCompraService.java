c12e89a841e40cf644e8c7281aeee668b0878cac
Diego Saouda <diego.saouda@forseit.com.br>
2017-05-17 11:48:40 -0300

    mock efetuarCompra

--- loja-api/src/main/java/com/github/dsaouda/fiap/webservice/loja/api/rest/EfetuarCompraService.java
+++ loja-api/src/main/java/com/github/dsaouda/fiap/webservice/loja/api/rest/EfetuarCompraService.java
@@ -12,13 +12,15 @@ import com.github.dsaouda.fiap.webservice.loja.api.model.Pedido;
 import com.github.dsaouda.fiap.webservice.loja.api.repository.ProdutoRepository;
 
 @Path("/efetuar-compra")
-public class EfetuarCompra {
+public class EfetuarCompraService {
 	
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	public boolean store(Pedido pedido) {		
 		//@TODO precisamos debitar do estoque?
+		//@TODO aguardando os outros grupos
+		
 		
 		//capturando maiores informações sobre o produto
 		//o cliente não envia detalhe dos produtos, apenas código
@@ -31,16 +33,23 @@ public class EfetuarCompra {
 		
 		System.out.println(pedido);
 		
-		
-		//@TODO aguardando os outros grupos
+		//verificando a quantidade de produtos em estoque
+		pedido.getListaProdutos().stream().forEach(p -> {
+			
+			//qtd estoque 0, solicitar ao grupo fornecedor, se o grupo não tiver o produto, retornar 0
+			if (p.getQuantidadeEstoque() == 0) {
+				//fornecedorClient.solicitarPedido()
+			}
+		});		
 		
 		//emitir nota fiscal com grupo governo
+		//boolean notaEmitida = governoClient.emitirNota();
 		
 		//valor total debitado do grupo financeira
+		//financeiraClient.debitarValor(valorTotal);
 		
 		//solicitacao de entrega ao grupo transportadora
-		
-		//qtd estoque 0, solicitar ao grupo fornecedor, se o grupo não tiver o produto, retornar 0
+		//transportadoraClient.solicitarEntrega(produtos);
 		
 		return true;
 	}
