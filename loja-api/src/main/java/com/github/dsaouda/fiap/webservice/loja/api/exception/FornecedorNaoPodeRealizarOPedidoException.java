b9c5920c0f467815cb0c17d0c8faf5402e34a452
Diego Saouda <dsaouda@gmail.com>
2017-05-20 02:00:31 -0300

    integracao com grupo fornecedor

---
+++ loja-api/src/main/java/com/github/dsaouda/fiap/webservice/loja/api/exception/FornecedorNaoPodeRealizarOPedidoException.java
@@ -0,0 +1,8 @@
+package com.github.dsaouda.fiap.webservice.loja.api.exception;
+
+public class FornecedorNaoPodeRealizarOPedidoException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	
+}
---
+++ loja-api/src/main/java/com/github/dsaouda/fiap/webservice/loja/api/exception/GovernoNaoPOdeEmitirNotaException.java
@@ -0,0 +1,7 @@
+package com.github.dsaouda.fiap.webservice.loja.api.exception;
+
+public class GovernoNaoPOdeEmitirNotaException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+}
